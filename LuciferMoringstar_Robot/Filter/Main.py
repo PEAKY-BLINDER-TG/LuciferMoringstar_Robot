a406f135972ad17f470117a41168a15bf116ff72
Peaky blinder <85233139+PEAKY-BLINDER-TG@users.noreply.github.com>
2021-10-14 08:20:39 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -310,7 +310,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
                     f_caption = f"{files.file_name}"
                 buttons = [
                     [
-                        InlineKeyboardButton('🖥️ How To Own 🖥️', url=f'{TUTORIAL}')
+                        InlineKeyboardButton('🚀 Support Group', url=f'{TUTORIAL}')
                     ]
                     ]
                 
